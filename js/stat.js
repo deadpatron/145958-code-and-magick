26baf98d4b9dea9363039764e3da2a13a71cf3c6
deadpatron <deadpatron@gmail.com>
2018-06-12 21:19:28 +0300

    Закончил второе задание, вроде всё норм

--- js/stat.js
+++ js/stat.js
@@ -7,22 +7,22 @@ var CLOUD_Y = 255;
 var GAP = 50;
 var BAR_WIDTH = 40;
 var barHeight = CLOUD_HEIGHT - GAP - GAP - BAR_WIDTH;
-var randomBlue = 'rgba(0, 0, 255, (Math.round(Math.random() * 10))/10)';
+var randomBlue = 'rgba(0, 0, 255, 1)';
 var POINT_GAP = 10;
 
 var renderCloud = function (ctx, color, startX, startY) {
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.moveTo(startX, startY);
-  ctx.lineTo((CLOUD_WIDTH + startX) / 4, startY + 10);
-  ctx.lineTo((CLOUD_WIDTH + startX) / 2, startY);
-  ctx.lineTo((CLOUD_WIDTH + startX) / 4 * 3, startY + 10);
-  ctx.lineTo((CLOUD_WIDTH + startX), startY);
-  ctx.lineTo((CLOUD_WIDTH + startX), CLOUD_HEIGHT + startY);
-  ctx.lineTo((CLOUD_WIDTH + startX) / 4 * 3, CLOUD_HEIGHT + startY + 10);
-  ctx.lineTo((CLOUD_WIDTH + startX) / 2, CLOUD_HEIGHT + startY);
-  ctx.lineTo((CLOUD_WIDTH + startX) / 4, CLOUD_HEIGHT + startY + 10);
-  ctx.lineTo(startX, CLOUD_HEIGHT);
+  ctx.lineTo(CLOUD_WIDTH / 4 + startX, startY + 10);
+  ctx.lineTo(CLOUD_WIDTH / 2 + startX, startY);
+  ctx.lineTo(CLOUD_WIDTH / 4 * 3 + startX, startY + 10);
+  ctx.lineTo(CLOUD_WIDTH + startX, startY);
+  ctx.lineTo(CLOUD_WIDTH + startX, CLOUD_HEIGHT + startY);
+  ctx.lineTo(CLOUD_WIDTH / 4 * 3 + startX, CLOUD_HEIGHT + startY + 10);
+  ctx.lineTo(CLOUD_WIDTH / 2 + startX, CLOUD_HEIGHT + startY);
+  ctx.lineTo(CLOUD_WIDTH / 4 + startX, CLOUD_HEIGHT + startY + 10);
+  ctx.lineTo(startX, CLOUD_HEIGHT + startY);
   ctx.lineTo(startX, startY);
   ctx.closePath();
   ctx.fill();
@@ -53,10 +53,14 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillStyle = '#000000';
     ctx.fillText(names[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y);
     ctx.fillStyle = randomBlue;
-    if (names[i] == 'Вы') {
+    ctx.globalAlpha = Math.round(Math.random() * 10) / 10 + 0.1;
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.globalAlpha = 1;
     }
-    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y - BAR_WIDTH, BAR_WIDTH, ((-barHeight) * times[i]) / maxTime);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, ((-barHeight * times[i]) / maxTime) + POINT_GAP);
+    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y - POINT_GAP, BAR_WIDTH, ((-barHeight) * times[i]) / maxTime);
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = '#000000';
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - (barHeight * times[i]) / maxTime - GAP);
   }
 };
